476fb19f92565869a179505314d15aec0b8fb96f
sujeetpandit2 <p.sujeet@mufin.co.kr>
2024-02-04 18:56:02 +0530

    background job or a message queue to handle email notifications asynchronously

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const { registerUser, loginUser, logoutUser } = require('../controllers/userController');
+const { registerUser, loginUser } = require('../controllers/userController');
 const {validateUser, validateLogin}  = require('../services/userServices');
-const {sendBulkEmail, allMails} = require('../controllers/bulkEmailApi');
 const { startEmailSendingController } = require('../queueServices/queues');
 const router = express.Router();
 
@@ -10,7 +9,6 @@ router.post('/register', validateUser, registerUser);
 router.post('/login', validateLogin, loginUser);
 
 //send bulk mail
-router.post('/sendBulkMail', sendBulkEmail)
 router.post('/mails', startEmailSendingController)
 
 
